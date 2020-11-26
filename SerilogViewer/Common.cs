a6947eed08476af62660ae8a0efcb70819aa0dc8
mak-thevar <mthevar@balendin.in>
2020-11-26 13:42:54 +0530

    Add project files.

---
+++ SerilogViewer/Common.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerilogViewer
+{
+    public class Common
+    {
+        public static ILogger GetLogger<T>() => new LoggerConfiguration()
+                  .WriteTo.File(new JsonFormatter(),
+                    AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + "/SeriLogs.txt", rollingInterval: RollingInterval.Day, 
+                    fileSizeLimitBytes: 5242880, rollOnFileSizeLimit: true, shared: true)
+                  .CreateLogger().ForContext<T>();
+    }
+}
---
+++ SerilogViewer/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SerilogViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger logger;
+        public HomeController()
+        {
+            logger = Common.GetLogger<HomeController>();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public int SomeTask()
+        {
+            try
+            {
+                var type = (100*10) - 1000;
+                var val = 10;
+                return val / type;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error occured in SomeTask");
+                throw;
+            }
+        }
+    }
+}
